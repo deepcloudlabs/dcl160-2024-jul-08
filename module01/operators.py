1c985f4d0bc24a2768a71041f94288ed7dc8fd81
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-09 14:24:22 +0300

    Create operators.py

---
+++ module01/operators.py
@@ -0,0 +1,10 @@
+x = 42  # 0010 1010
+y = 108 # 0110 1100
+print(x + y)
+print(x - y)
+print(x * y)
+print(x // y)
+print(x / y)
+print(x ^ y)  # bitwise xor 0100 0110 -> 70
+print(x & y)  # bitwise and 0010 1000 -> 40
+print(x | y)  # bitwise or  0110 1110 -> 110
