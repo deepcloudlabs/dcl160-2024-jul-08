b54cf44b467ec1809cb0bf122391607960999cce
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-09 11:15:59 +0300

    Create exercise01.py

---
+++ module04/exercise01.py
@@ -0,0 +1,11 @@
+"""
+i. if
+   if/else
+   if/elif/else
+   if/elif/elif/else
+   condition -> if, elif -> True/False
+
+ii. loops:
+    a. regular loops: for -> iterables: tuple/list/set/dictionary/str/...
+    b. irregular loops: while -> condition -> True/False
+"""
