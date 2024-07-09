a505a2b6b9b18d0ae73cae271de6c6a968120fb9
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-09 11:42:49 +0300

    Create exercise03.py

---
+++ module04/exercise03.py
@@ -0,0 +1,14 @@
+"""
+Leap year Test
+2000 Leap Year          -> 4
+2001 Not a Leap Year    
+2002 Not a Leap Year
+2003 Not a Leap Year
+2004 Leap Year
+
+2100 Not a Leap Year -> 100
+2200 Not a Leap Year -> 100
+2300 Not a Leap Year -> 100
+2400 Leap Year       -> 400
+"""
+number = int(input("Enter a year: ").strip())
