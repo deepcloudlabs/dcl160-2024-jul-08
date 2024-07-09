550756c6d454ceb7de624f17037d7457d399cb48
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-09 12:08:47 +0300

    Create exercise05.py

---
+++ module04/exercise05.py
@@ -0,0 +1,23 @@
+"""
+Leap year Test
+2000 Leap Year          -> 4
+2001 Not a Leap Year
+2002 Not a Leap Year
+2003 Not a Leap Year
+2004 Leap Year
+
+2100 Not a Leap Year -> 100
+2200 Not a Leap Year -> 100
+2300 Not a Leap Year -> 100
+2400 Leap Year       -> 400
+"""
+# 11:53
+year = int(input("Enter a year: ").strip())
+if not year % 4 == 0:
+    print(year, "is Not a Leap Year")
+elif not year % 100 == 0:
+    print(year, "is a Leap Year")
+elif not year % 400 == 0:
+    print(year, "is Not a Leap Year")
+else:
+    print(year, "is a Leap Year")
