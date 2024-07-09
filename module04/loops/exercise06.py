638e6b010c592ce7a7e401522d03642c77269d3d
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-09 14:11:26 +0300

    Update exercise06.py

--- module04/loops/exercise06.py
+++ module04/loops/exercise06.py
@@ -3,7 +3,18 @@
  starting from 1. He says that the sum of the house numbers less than
  his house number is equal to the sum of the house numbers greater
  than his house number. Knowing that his house number contains
- 3 digits design an algorithm
- and write a Python script to find his house number.
+ 3 digits design an algorithm and write a Python script
+ to find his house number.
  """
-
+for mathematician_building_no in range(100, 1000):
+    left_sum = 0
+    right_sum = 0
+    for building_no in range(1, mathematician_building_no - 1):
+        left_sum += building_no
+    building_no = mathematician_building_no + 1
+    while right_sum < left_sum:
+        right_sum += building_no
+        building_no += 1
+    if right_sum == left_sum:
+        print(mathematician_building_no, building_no - 1)
+        break
