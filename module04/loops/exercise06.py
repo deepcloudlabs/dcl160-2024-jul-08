5e3f9b6cffe375061cb4eaa7127ae2c54efe2503
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-09 13:59:35 +0300

    Create exercise06.py

---
+++ module04/loops/exercise06.py
@@ -0,0 +1,9 @@
+"""
+ A mathematician lives on a street where house numbers are consecutive
+ starting from 1. He says that the sum of the house numbers less than
+ his house number is equal to the sum of the house numbers greater
+ than his house number. Knowing that his house number contains
+ 3 digits design an algorithm
+ and write a Python script to find his house number.
+ """
+
