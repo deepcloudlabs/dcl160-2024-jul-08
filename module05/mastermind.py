db9a3a8a12cae8e8c26a307f330989f77a962a2f
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-10 10:26:30 +0300

    Create mastermind.py

---
+++ module05/mastermind.py
@@ -0,0 +1,18 @@
+"""
+level: 3
+live: 3
+maxMoves: 10
+secret: 549
+player -> 123
+          No match
+          456
+          -2
+          574
+          +1-1
+          ...
+          549
+level: 4
+secret: 3615
+...
+level:10
+"""
