2baa5ed5085d337d063b194f9e3d29a1823e9c70
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-11 10:32:33 +0300

    Update exercise01.py

--- module07/exercise01.py
+++ module07/exercise01.py
@@ -8,4 +8,12 @@ File -> Persistence
 data -> memory: list/set/tuples/dict -> persist -> file
         file: i. text i/o
              ii. binary i/o
+
+Serialization (structured data):
+1. csv, json, xml -> text i/0 -> hrf
+   cross platform
+2. pickle -> binary -> python
+3. protocol buffers -> binary -> cross platform
+   cncf: https://www.cncf.io/
+   parquet -> binary -> big data analytics, dwh
 """
