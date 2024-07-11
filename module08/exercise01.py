fe3d224149f871aaff90664421cb8369522a9d2b
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-11 11:11:47 +0300

    Create exercise01.py

---
+++ module08/exercise01.py
@@ -0,0 +1,19 @@
+"""
+Modeling
+1. Procedural Programming -> functions -> data: global variables
+   x = 42
+   def fun():
+      # global x
+      x = 108
+   structured programming: if/elif/else/match, while/for
+   imperative programming
+2. Functional Programming -> functions -> HoF, Pure -> Function Composition -> pipeline
+   data -> pipeline -> solution
+   HoF: filter/map/flatMap/reduce(sum/min/max)/...
+   declarative programming
+3. OOP -> Class -> Object
+     i. Members: attribute, method -> encapsulation
+      [ data + methods ]
+    ii. Inheritance -> reusability
+   iii. Polymorphism -> agility
+"""
