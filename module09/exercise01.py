737aa6a6ece4f876a633b81e6c9c9514b1c3728a
Binnur KURT <deepcloudlabs@gmail.com>
2024-07-11 12:27:04 +0300

    Create exercise01.py

---
+++ module09/exercise01.py
@@ -0,0 +1,11 @@
+"""
+Regular Expression: Language
+e-mail -> language
+zipcode -> language
+tc kimlik no,phone,license plate -> language
+regular expression -> parser -> language
+word ∈ language ?
+validation, parsing
+I.  . * + ? {} [] ()
+II. re
+"""
